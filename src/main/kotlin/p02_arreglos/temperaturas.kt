9b100967cd207c481065472e879cb5c44c5d0ab6
josegarcia19809 <josegarcia19809@gmail.com>
2026-03-26 08:16:12 -0600

    Arreglo: climas

---
+++ src/main/kotlin/p02_arreglos/temperaturas.kt
@@ -0,0 +1,19 @@
+package org.example.p02_arreglos
+
+fun main() {
+    println("Temperaturas semanales en texto ☀️🌧️")
+
+    val climas = Array(7) { "" }
+    for (i in 0..6) {
+        print("Dame la descripción del clima ${i + 1}: ")
+        climas[i] = readln()
+    }
+
+    println()
+    for (clima in climas) {
+        print("$clima \t")
+    }
+    
+    println()
+    println("Total de descripciones: ${climas.size}")
+}

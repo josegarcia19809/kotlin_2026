d1766c9d2636a01155163567ec525c3ac6d6d33a
josegarcia19809 <josegarcia19809@gmail.com>
2026-06-18 09:11:56 -0600

    Ejercicio para examen: respuesta

--- src/main/kotlin/exaamen.kt
+++ src/main/kotlin/exaamen.kt
@@ -1,4 +1,29 @@
 package org.example
 
+class Empleado(val nombre: String, val edad: Int, var salario: Double) {
+    fun aumentarSalario(porcentaje: Double) {
+        val incremento = salario * (porcentaje / 100.0)
+        salario = salario + incremento
+    }
 
+    fun obtenerInformacion(): String {
+        val info = "Nombre: $nombre, edad: $edad, salario: $salario"
+        return info
+    }
+}
 
+fun main() {
+
+    val empleado1 = Empleado("Juan", 25, 12000.0)
+    val empleado2 = Empleado("Ana", 30, 15000.0)
+
+    println("=== Empleado 1 ===")
+    println(empleado1.obtenerInformacion())
+    empleado1.aumentarSalario(10.0)
+    println(empleado1.obtenerInformacion())
+
+    println("\n=== Empleado 2 ===")
+    println(empleado2.obtenerInformacion())
+    empleado2.aumentarSalario(10.0)
+    println(empleado2.obtenerInformacion())
+}
